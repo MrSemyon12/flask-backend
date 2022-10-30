78c7b9ea0096d17fb68fe1ba797f23dbf79239a5
MrSemyon12 <denisenko.box19@gmail.com>
2022-10-30 23:56:35 +1000

    movie

--- controllers/movie.py
+++ controllers/movie.py
@@ -1,5 +1,5 @@
-from models.movie_model import get_film
-from flask import render_template, request, session
+from models.movie_model import get_film, get_film_director, get_film_actors, get_reviews
+from flask import render_template
 from app import app
 
 from utils import get_db_connection
@@ -9,4 +9,11 @@ conn = get_db_connection()
 
 @app.route('/movie/<int:id>', methods=['GET'])
 def movie(id):
-    return render_template('movie.html', film=get_film(conn, id), len=len)
+    return render_template(
+        'movie.html',
+        film=get_film(conn, id),
+        director=get_film_director(conn, id),
+        actors=get_film_actors(conn, id),
+        reviews=get_reviews(conn, id),
+        len=len
+    )
--- models/movie_model.py
+++ models/movie_model.py
@@ -3,13 +3,38 @@ import pandas as pd
 
 def get_film(conn, film_id):
     return pd.read_sql('''
-        SELECT title, year, poster_url, rating, director_name, group_concat(DISTINCT genre_name), group_concat(DISTINCT actor_name)
+        SELECT title, year, poster_url, rating, group_concat(DISTINCT genre_name) AS genres
+        FROM film        
+        JOIN film_genre USING (film_id)
+        JOIN genre USING (genre_name)        
+        WHERE film_id == :id
+        GROUP BY film_id
+''', conn, params={'id': film_id})
+
+
+def get_film_director(conn, film_id):
+    return pd.read_sql('''
+        SELECT director_name, photo_url
         FROM film
         JOIN director USING (director_id)
-        JOIN film_genre USING (film_id)
-        JOIN genre USING (genre_name)  
+        WHERE film_id == :id
+''', conn, params={'id': film_id})
+
+
+def get_film_actors(conn, film_id):
+    return pd.read_sql('''
+        SELECT actor_name, photo_url
+        FROM film
         JOIN film_actor USING (film_id)
         JOIN actor USING (actor_id)
         WHERE film_id == :id
-        GROUP BY film_id
+''', conn, params={'id': film_id})
+
+
+def get_reviews(conn, film_id):
+    return pd.read_sql('''
+        SELECT user_name, post_date, comment, mark
+        FROM film
+        JOIN review USING (film_id)        
+        WHERE film_id == :id
 ''', conn, params={'id': film_id})
