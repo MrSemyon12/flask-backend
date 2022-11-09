9a6343621a4a0e4faefdf250bd23ac1865406eb6
MrSemyon12 <denisenko.box19@gmail.com>
2022-11-09 12:27:47 +1000

    +model

--- website/models.py
+++ website/models.py
@@ -4,5 +4,29 @@ from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), unique=True)
+    name = db.Column(db.String(25), unique=True)
     password = db.Column(db.String(255))
+
+
+class Director(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(25))
+    photo_url = db.Column(db.String(255))
+
+
+class Genre(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(25))
+
+
+class Actor(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(25))
+    photo_url = db.Column(db.String(255))
+
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(45))
+    year = db.Column(db.Integer)
+    poster_url = db.Column(db.String(255))
