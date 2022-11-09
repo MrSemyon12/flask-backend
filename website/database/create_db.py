f325606d2794e3342c2ca892909b2f7510dfaf93
MrSemyon12 <denisenko.box19@gmail.com>
2022-11-09 16:08:38 +1000

    +models

--- website/database/create_db.py
+++ website/database/create_db.py
@@ -3,16 +3,24 @@ import pandas as pd
 
 con = sqlite3.connect('website/filmoteka.db')
 
-# f_damp = open('website/database/database.db', 'r', encoding='utf-8-sig')
+# f_damp = open('website/database/script.db', 'r', encoding='utf-8-sig')
 # damp = f_damp.read()
 # f_damp.close()
 
 # con.executescript(damp)
 # con.commit()
 
-cursor = con.cursor()
+# cursor = con.cursor()
+# df = pd.read_sql('''
+#     SELECT sql
+#     FROM sqlite_schema
+#     WHERE name = 'director';
+# ''', con)
+
 df = pd.read_sql('''
-    SELECT * FROM user
+    SELECT *
+    FROM
+        director       
 ''', con)
 
 print(df)
--- website/models.py
+++ website/models.py
@@ -1,5 +1,6 @@
 from . import db
 from flask_login import UserMixin
+from datetime import datetime
 
 
 class User(db.Model, UserMixin):
@@ -30,3 +31,25 @@ class Movie(db.Model):
     title = db.Column(db.String(45))
     year = db.Column(db.Integer)
     poster_url = db.Column(db.String(255))
+    director_id = db.Column(db.Integer, db.ForeignKey('director.id'))
+
+
+class Movie_actor(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    movie_id = db.Column(db.Integer, db.ForeignKey('movie.id'))
+    actor_id = db.Column(db.Integer, db.ForeignKey('actor.id'))
+
+
+class Movie_genre(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    movie_id = db.Column(db.Integer, db.ForeignKey('movie.id'))
+    genre_id = db.Column(db.Integer, db.ForeignKey('genre.id'))
+
+
+class Review(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    movie_id = db.Column(db.Integer, db.ForeignKey('movie.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    post_date = db.Column(db.DateTime, default=datetime.utcnow)
+    comment = db.Column(db.Text, nullable=False)
+    mark = db.Column(db.Integer)
