93a916879aac43e2072157374cd7a884a2a5ecb9
MrSemyon12 <denisenko.box19@gmail.com>
2022-11-21 17:44:03 +1000

    fixed database

--- website/database/create_db.py
+++ website/database/create_db.py
@@ -3,26 +3,32 @@ import pandas as pd
 
 con = sqlite3.connect('website/filmoteka.db')
 
-f_damp = open('website/database/script.db', 'r', encoding='utf-8-sig')
-damp = f_damp.read()
-f_damp.close()
+# f_damp = open('website/database/script.db', 'r', encoding='utf-8-sig')
+# damp = f_damp.read()
+# f_damp.close()
 
-con.executescript(damp)
-con.commit()
+# con.executescript(damp)
+# con.commit()
 
 # cursor = con.cursor()
+
 # df = pd.read_sql('''
 #     SELECT sql
 #     FROM sqlite_schema
 #     WHERE name = 'director';
 # ''', con)
+# df = pd.read_sql('''
+#     SELECT poster_url
+#     FROM movie
+#     WHERE title = 'Форсаж'
 
+# ''', con)
 df = pd.read_sql('''
     SELECT name
-    FROM 
+    FROM
         sqlite_schema
-    WHERE 
-        type ='table' AND name NOT LIKE 'sqlite_%';      
+    WHERE
+        type ='table' AND name NOT LIKE 'sqlite_%';
 ''', con)
 
 print(df)
